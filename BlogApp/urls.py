24f6ba5bee886603dc701efeac1a55e1319c1e98
WaseeKhan <mdvaseem014@gmail.com>
2025-04-10 01:17:00 +0530

    home page UI setup

--- BlogApp/urls.py
+++ BlogApp/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+import blogging
+from django.views.generic.base import RedirectView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("blogs/", include('blogging.urls')),
+    path("", RedirectView.as_view(url='/blogs/', permanent=True)),
 ]
