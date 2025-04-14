5d5e8a353a253e5dafa2af5f404fc5dfd05f517c
WaseeKhan <mdvaseem014@gmail.com>
2025-04-15 00:37:22 +0530

    favicon etc

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import Category, Tag, Post, Comment
+from ckeditor.widgets import CKEditorWidget
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
