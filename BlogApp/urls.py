5776571f3d8e4c3d4a7f9afe6a02d40b9b080cb7
WaseeKhan <mdvaseem014@gmail.com>
2025-04-14 22:53:43 +0530

    App name changes from blogging to blog

--- BlogApp/urls.py
+++ BlogApp/urls.py
@@ -16,13 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-import blogging
+import blog
 from django.views.generic.base import RedirectView
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("blog/", include('blogging.urls')),
+    path("blog/", include('blog.urls')),
     path("", RedirectView.as_view(url='/blog/')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
